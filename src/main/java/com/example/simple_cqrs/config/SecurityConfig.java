6f3d83b9e63207d2242e941666d0cb4eac1e0726
NazeehAlsaifi <nazeeh.saifi@gmail.com>
2025-11-08 19:01:42 +0400

    adding logging and metrics configuration

--- src/main/java/com/example/simple_cqrs/config/SecurityConfig.java
+++ src/main/java/com/example/simple_cqrs/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll())
 
 //                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
