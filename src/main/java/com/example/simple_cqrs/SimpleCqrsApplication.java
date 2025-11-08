de8b51ad04ce3bd0506385f3c55c6e1308f1b52f
NazeehAlsaifi <nazeeh.saifi@gmail.com>
2025-11-08 11:38:30 +0400

    init commit

---
+++ src/main/java/com/example/simple_cqrs/SimpleCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCqrsApplication.class, args);
+	}
+
+}
